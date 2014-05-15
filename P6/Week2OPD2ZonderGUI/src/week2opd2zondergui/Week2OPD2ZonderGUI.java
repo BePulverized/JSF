a2f3f857ceb90990b796d2501aa53827fe1c7369
phinux <phinux@Wall-E>
2014-05-15 21:50:50 +0200

    toevoegen opdracht en aangeleverde klasse

--- P6/Week2OPD2ZonderGUI/src/week2opd2zondergui/Week2OPD2ZonderGUI.java
+++ P6/Week2OPD2ZonderGUI/src/week2opd2zondergui/Week2OPD2ZonderGUI.java
@@ -10,6 +10,11 @@ package week2opd2zondergui;
 public class Week2OPD2ZonderGUI {
 
     /**
+     * Een variant zonder GUI (dus een console applicatie) die de gebruiker vraagt voor welk level de edges gegenereerd moeten worden, en die edges vervolgens in een file zet (dus niet in een array, en ook niet tekenen).  
+Het level moet ook in de file staan. 
+Voor deze variant worden alleen de klassen KochFractal en Edge uit het vorige kwartaal gebruikt. 
+De betreffende file moet zich op de hierboven gemounte nieuwe disk bevinden. 
+Zet zonodig de permissions goed. Geef hieronder aan welke file (inclusief pad) er precies gebruikt is en welke permissions je aan moest passen.
      * @param args the command line arguments
      */
     public static void main(String[] args) {
